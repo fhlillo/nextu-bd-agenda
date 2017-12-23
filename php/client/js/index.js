1eae65d1b4d88240ccbb21f751e87ae0301ca55a
fhlillo <f.guicharrousse@gmail.com>
2017-12-22 22:40:30 -0300

    archivso clientes

--- php/client/js/index.js
+++ php/client/js/index.js
@@ -1,42 +1,43 @@
 $(function(){
   var l = new Login();
-})
+});
 
 
 class Login {
   constructor() {
-    this.submitEvent()
+    this.submitEvent();
   }
 
   submitEvent(){
-    $('form').submit((event)=>{
-      event.preventDefault()
-      this.sendForm()
-    })
+    $('form').submit((event) => {
+      event.preventDefault();
+      this.sendForm();
+    });
   }
 
   sendForm(){
     let form_data = new FormData();
-    form_data.append('username', $('#user').val())
-    form_data.append('password', $('#password').val())
+    form_data.append('username', $('#user').val());
+    form_data.append('password', $('#password').val());
     $.ajax({
-      url: '../server/check_login.php',
+      url: '../server/login.php',
       dataType: "json",
       cache: false,
       processData: false,
       contentType: false,
       data: form_data,
       type: 'POST',
-      success: function(php_response){
-        if (php_response.msg == "OK") {
+      success: (data) => {
+        if (data.message == "OK") {
           window.location.href = 'main.html';
-        }else {
-          alert(php_response.msg);
+        } else {
+          alert(data.description);
         }
       },
-      error: function(){
-        alert("error en la comunicación con el servidor");
+      error: (data) => {
+        alert("Error en la comunicación con el servidor");
+        console.log(data);
       }
-    })
+    });
   }
 }
